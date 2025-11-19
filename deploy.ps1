f4678592b00803377e4392e7bc7f4e3d136d562c
Austin Pauley <austin2118ace@gmail.com>
2025-11-19 15:47:55 -0500

    Colorful header

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,13 @@
+# Synology Drive Client Deployment Script
+# Fill $INSTALLER_PATH with path to the installer EXE
+
+$INSTALLER_PATH = 'PATH/TO/INSTALLER.EXE'
+
+Write-Host "Synology Drive Client Mass Deployment Script" -ForegroundColor Cyan
+Write-Host "Austin Pauley, Florida State University, 2025" -ForegroundColor Cyan
+Write-Host "=============================================`n" -ForegroundColor Cyan
+Write-Host "Adding computer name to backup destination path" -ForegroundColor Yellow
+
 $config_file = Get-Content ".\config.json" -raw | ConvertFrom-Json
 $existing_path = $config_file.connections.backup_task.backup_destination.remote_path
 $new_path = $existing_path + $env:COMPUTERNAME + '/'
