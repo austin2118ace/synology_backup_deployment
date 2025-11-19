90b9d87c78a9de39d7cc09315bc6c82d8149e142
Austin Pauley <austin2118ace@gmail.com>
2025-11-19 15:48:23 -0500

    Update path

--- deploy.ps1
+++ deploy.ps1
@@ -11,9 +11,8 @@ Write-Host "Adding computer name to backup destination path" -ForegroundColor Ye
 $config_file = Get-Content ".\config.json" -raw | ConvertFrom-Json
 $existing_path = $config_file.connections.backup_task.backup_destination.remote_path
 $new_path = $existing_path + $env:COMPUTERNAME + '/'
-$config_file.connections.backup_task.backup_destination.remote_path = $new_path
-
-Write-Host $config_file.connections.backup_task.backup_destination.remote_path
+$config_file.connections.backup_task.backup_destination.remote_path = $new_path # Set key to new path
+Write-Host "New Remote Backup Path $($config_file.connections.backup_task.backup_destination.remote_path)" # Print new path
 
 $config_file | ConvertTo-Json -depth 100 | set-content ".\custom_config.json"
 
